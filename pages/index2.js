4bf10e426bf341f68c091d0d5acd72864991570a
David Adiel <davidadiel96@gmail.com>
2023-01-12 07:59:17 +0700

    menambahkan semua file local

--- pages/index2.js
+++ pages/index2.js
@@ -163,6 +163,76 @@ export default function Home() {
           </div>
         </section>
         <section>
+          <div className=" dark:text-white">
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-80">
+              Since the beginning of my journey as a freelancer developer, I've
+              done a remote work for
+              <span className="text-orange-500"> agencies </span>
+              consulted for <span className="text-orange-500">startups </span>
+              and collaborated with talented people to create for both business
+              and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              I offer from a wide range of services, including programming and
+              &#9834; music arranger.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+              <Image
+                className="mx-auto"
+                src={design}
+                width={100}
+                height={100}
+              />
+              <h3 className="text-xl font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following design
+                theory.
+              </p>
+              <h4 className="py-4 text-orange-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+              <Image className="mx-auto" src={code} width={100} height={100} />
+              <h3 className="text-xl font-medium pt-8 pb-2">Clean</h3>
+              <p className="py-2">
+                Creating clean and good functional features.
+              </p>
+              <h4 className="py-4 text-orange-600">Language I use</h4>
+              <p className="text-gray-800 py-1">NodeJS</p>
+              <p className="text-gray-800 py-1">HTML & CSS</p>
+              <p className="text-gray-800 py-1">Javascript</p>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">Ionic</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+              <Image
+                className="mx-auto"
+                src={consulting}
+                width={100}
+                height={100}
+              />
+              <h3 className="text-xl font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following design
+                theory.
+              </p>
+              <h4 className="py-4 text-orange-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+        <section>
           <div className="dark:text-white">
             <h3 className="text-3xl py-1">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-80">
