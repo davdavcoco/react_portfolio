53cd61879c3722b8fee56ff7a5ff1aba521003bc
David Adiel <davidadiel96@gmail.com>
2023-01-13 09:54:15 +0700

    update index and package

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
               <li className="">
                 <a
                   className="bg-gradient-to-r from-orange-500 to-red-500 text-white px-4 py-2 rounded-md ml-8"
-                  href=""
+                  href="https://www.linkedin.com/in/david-adiel-1b9187243/"
                 >
                   Resume
                 </a>
